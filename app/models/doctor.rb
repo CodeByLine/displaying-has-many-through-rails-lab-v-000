12104438e92947b79628448d6e877887ebfdcc1d
CodeByLine <yleventhal@ymail.com>
2016-08-09 14:31:01 -0400

    weird new folder

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,10 @@
 class Doctor < ActiveRecord::Base
   has_many :appointments
   has_many :patients, through: :appointments
+
+  # def doctor_name=(name)
+  #   self.appointment = Appointment.find_or_create_by(appointment_datetime)
+  # end
+
+  # end
 end
